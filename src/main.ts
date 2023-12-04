62d7750dc6c5b0bdc8dc715ee4255516e999fa92
m0slah <muhammedslahqadir@gmail.com>
2023-12-04 21:19:23 +0300

    first commit

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+let username = "mo";
+
+console.log(username);
+
+let a: number;
+let b: string;
+let c: number;
+
+a = 2;
+b = "3";
+c = 1;
+
+let r = a / b;
+let r2 = c * b;
+
+console.log(r);
+console.log(r2);
